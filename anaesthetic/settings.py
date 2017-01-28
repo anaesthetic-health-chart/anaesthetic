d55789a54a1b59fade24eb7eb64551896b858ad2
fredkingham <fredkingham@gmail.com>
2017-01-28 15:17:05 +0000

    add in read me and token authentication

--- anaesthetic/settings.py
+++ anaesthetic/settings.py
@@ -172,6 +172,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
