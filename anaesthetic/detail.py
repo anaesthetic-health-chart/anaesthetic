9bc8c28d453043c8bd698f9341cbe8f310efc2c2
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2017-10-26 15:41:43 +0100

    commit

--- anaesthetic/detail.py
+++ anaesthetic/detail.py
@@ -1,17 +1,14 @@
-"""
-The custom Micro Haem detail view.
-"""
 from opal.core import detail
 from opal.models import UserProfile
 
 class AnaestheticReadings(detail.PatientDetailView):
     order = 1
     name = 'anaesthetic_reading'
-    display_name = 'Anaesthetic Readings'
+    display_name = 'Anaesthetic Chart'
+    # title = 'Anaesthetic Readings'
     template   = 'anaesthetic/detail/reading_detail.html'
 
 class PreopAssessment(detail.PatientDetailView):
-    order   = 2
-    name    = 'Pre_Op_assessment'
+    name    = 'preop_assessment'
     display_name    = 'Pre Op Assessment'
     template        = 'anaesthetic/detail/preopassessment.html'
